bd6934c34ae559a011aa39fa624061a51a8b18ca
Ben Evans <ben@bensbit.co.uk>
2016-08-02 12:26:44 +0100

    test

---
+++ test.js
@@ -0,0 +1,11 @@
+import test from 'ava'
+import screenshot from './'
+import { isBuffer } from 'util'
+
+test(t => {
+  t.plan(1)
+  return screenshot().then(img => {
+    t.truthy(isBuffer(img))
+  })  
+})
+
