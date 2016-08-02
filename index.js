2b9a4a734395f701fec891f3699519f7ef5e2afe
Ben Evans <ben@bensbit.co.uk>
2016-08-02 12:04:07 +0100

    structure

--- index.js
+++ index.js
@@ -1,54 +1,14 @@
-const exec = require('child_process').exec
-const Promise = require('pinkie-promise')
-const fs = require('fs');
-const temp = require('temp')
-const execOptions = {
-  encoding: 'buffer',
-  maxBuffer: 1024 * 1000
-}
 
-function desktop () {
-  return new Promise((resolve, reject) => {
-
-    if (process.platform == 'linux') {
-      exec('import -silent -window root jpeg:- ', execOptions,
-      function(err, stdout) {
-        if (err) {
-          return reject(err)
-        } else {
-          return resolve(stdout)
-        }
-      })
-    }
-    else if (process.platform === 'darwin' {
-      exec('screencapture -t jpg tmpfile', execOptions, function (err, stdOut) {
-        if (err) {
-          return reject(err)
-        } else {
-          let tmpPath = temp.path({suffix: '.jpg'})
-          fs.readFile(tmpPath, function (err, img) {
-            if (err) {
-              return reject(err)
-            }
-            fs.unlink(tmpPath, function (err) {
-              if (err) {
-                return reject(err)
-              }
-              reutrn accept(img)
-            })
-          })
-          return accept(stdOut)
-        }
-      }
-    }
-    else if (process.platform === 'windows') {
-
-    } else {
+if (process.platform == 'linux') {
+  module.exports = require('./lib/linux')
+} else if (process.platform === 'darwin') {
+  module.exports = require('./lib/darwin')
+} else if (process.platform === 'windows') {
+  module.exports = require('./lib/windows')
+} else {
+  module.exports = function unSupported () {
+    return new Promise((resolve, reject) => {
       return reject(new Error('Currently unsupported platform. Pull requests welcome!'))
-    }
-
-
-  })
+    })
+  }
 }
-
-module.exports = desktop
