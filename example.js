1b18704843be89aaa59a4efce3d6a48cc4ba5ac4
zhanggang <zhanggang@ciphertrading.com>
2018-11-14 13:45:45 +0800

    link and fix delete file error

--- example.js
+++ example.js
@@ -1,17 +1,29 @@
+const path = require('path')
 const screenshot = require('./')
-const fs = require('fs')
-const path = require('path');
-console.log(__dirname);
 
-screenshot.listDisplays().then((displays) => {
+screenshot.listDisplays()
+  .then((displays) => {
+    console.log(displays)
+    for (let index = 0; index < displays.length; index++) {
+      const display = displays[index]
+      const imgpath = path.join(__dirname, Date.now() + '_' + index + '.png')
+      screenshot({ screen: display.id, filename: imgpath }).then((imgbuf) => {
+        console.log(imgbuf)
+      }).catch(err => {
+        console.error(err)
+      })
+    }
+  })
 
-  console.log(displays);
-  for (let index = 0; index < displays.length; index++) {
-    const display = displays[index];
-    const imgpath = path.join(__dirname, Date.now() + '_' + display.id + '.png')
-    screenshot({ screen: display.id, filename: imgpath }).then((imgpath) => {
-      console.log(imgpath);
-    })
-  }
-
-})
+screenshot.listDisplays()
+  .then((displays) => {
+    console.log(displays)
+    for (let index = 0; index < displays.length; index++) {
+      const display = displays[index]
+      screenshot({ screen: display.id }).then((imgbuf) => {
+        console.log(imgbuf)
+      }).catch(err => {
+        console.error(err)
+      })
+    }
+  })
